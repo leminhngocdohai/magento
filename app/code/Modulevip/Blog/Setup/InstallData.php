79f73b2f2fbb1a0e80cf761db5276368211300a1
Minh Ngoc <leminhngocdohai@gmail.com>
2021-01-26 10:57:06 +0700

    setup

--- app/code/Modulevip/Blog/Setup/InstallData.php
+++ app/code/Modulevip/Blog/Setup/InstallData.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Modulevip\Blog\Setup;
+
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
@@ -8,16 +10,27 @@ class InstallData implements InstallDataInterface
 {
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        $data = [
-            [
-                'title' => 'Post 1 Title',
-                'content' => 'Chào mừng đến với the first post.',
-            ],
+        $setup->startSetup();
 
-        ];
+        if ($context->getVersion()) {
+            $tableName = $setup->getTable('modulevip_blog_post');
 
-        foreach ($data as $value) {
-            $setup->getConnection()->insertForce($setup->getTable('modulevip_blog_post'), $value);
+            $data = [
+                [
+                    'title' => 'Post 1 Title',
+                    'content' => 'Chào mừng đến với the first post.',
+                ],
+                [
+                    'title' => 'Post 2 Title',
+                    'content' => 'Chào mừng đến với the second post.',
+                ],
+            ];
+
+            $setup
+                ->getConnection()
+                ->insertMultiple($tableName, $data);
         }
+
+        $setup->endSetup();
     }
 }
